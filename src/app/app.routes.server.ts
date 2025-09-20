91101b61c719bac9e8ad7c48baabf1a88a1c7c67
Mark <markehabtawfiq@gmail.com>
2025-09-20 14:32:16 +0300

    app.routes.server.ts | Disable prerendering on Checkout and ProductDetails components

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,11 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'productdetails/:slug_name/:id', renderMode: RenderMode.Server },
+  { path: 'productdetails/:id', renderMode: RenderMode.Server },
+  { path: 'checkout/:id', renderMode: RenderMode.Server },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
