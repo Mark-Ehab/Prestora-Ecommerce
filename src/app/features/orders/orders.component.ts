af0ff1b571cc273dddc8268e28a1045b0fa5443e
Mark <markehabtawfiq@gmail.com>
2025-09-22 22:17:39 +0300

    orders.component.ts | Remove unwanted code

--- src/app/features/orders/orders.component.ts
+++ src/app/features/orders/orders.component.ts
@@ -21,43 +21,4 @@ export class OrdersComponent {
 
   /* Properties */
   allOrders: Order[] = this.activatedRoute.snapshot.data['ordersList'];
-
-  /* Properties */
-  // private signinDecodedToken: DecodedSigninToken | undefined =
-  //   {} as DecodedSigninToken;
-  // private getSpecificUserOrdersSubscription!: Subscription;
-  // allOrders: Order[] = [] as Order[];
-
-  /* Methods */
-  /*-----------------------------------------------------------------------------
-  # Description: A function to get the data of all orders of a logged user through 
-  # Route E-Commerce API on '/orders/user' endpoint
-  #------------------------------------------------------------------------------
-  # @params: 
-  # @param 1: userId: string | undefined
-  #------------------------------------------------------------------------------
-  # return type: Observable<any>
-  -----------------------------------------------------------------------------*/
-  // getSpecificUserOrdersData(userId: string | undefined): void {
-  //   this.getSpecificUserOrdersSubscription = this.ordersService
-  //     .getSpecificUserOrders(userId)
-  //     .subscribe({
-  //       next: (response) => {
-  //         this.allOrders = response;
-  //       },
-  //       error: (err) => console.log('%c Error: ', 'color:red', err.message),
-  //     });
-  // }
-
-  // /* Component Lifecycle Hooks */
-  // ngOnInit(): void {
-  //   /* Get decoded signin token */
-  //   this.signinDecodedToken = this.authenticationService.decodeSigninToken();
-  //   /* Get specific user orders on component initialization */
-  //   this.getSpecificUserOrdersData(this.signinDecodedToken?.id);
-  // }
-  // ngOnDestroy(): void {
-  //   /* Unsubscribe from getSpecificUserOrdersSubscription subscription on component destruction */
-  //   this.getSpecificUserOrdersSubscription.unsubscribe();
-  // }
 }
